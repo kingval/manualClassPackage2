a8c4f086eae679c4b722c5b3071d92111737d8fb
kingval <king.valand@gmail.com>
2022-12-04 23:50:30 +0000

    manual-Package2-upload

---
+++ ManualClassWeek4.java
@@ -0,0 +1,18 @@
+package manualClassPackage2;
+
+public class ManualClassWeek4 {
+    public static void main(String[] args)
+    {
+        System.out.println("Manual Testing Week 4\n" +
+                "* Static vs Dynamic Testing\n" +
+                "* Exploratory Testing\n" +
+                "* Ad-Hoc Testing\n" +
+                "* Software Development Models\n" +
+                "* Waterfall\n" +
+                "* Iterative\n" +
+                "* V-Model\n" +
+                "* Verification and Validation\n" +
+                "* Agile & Scrum Framework \n" +
+                "* Definition of Ready/Done\n");
+    }
+}
---
+++ ManualClassWeek5.java
@@ -0,0 +1,15 @@
+package manualClassPackage2;
+
+public class ManualClassWeek5 {
+    public static void main(String[] args)
+    {
+        System.out.println("Manual Testing Week 5\n" +
+                "* What is TDD?\n" +
+                "* What is BDD?\n" +
+                "* BDD Tools\n" +
+                "* What is Gherkin?\n" +
+                "* BDD keywords\n" +
+                "* How to write test cases with BDD?\n" +
+                "* Maintenance");
+    }
+}
